bd7193ce3bdf848794cd74265bc36e51d83d8190
Ojas Joshi <ojasjoshi.work@gmail.com>
2020-08-05 16:05:37 +0530

    Added Models

--- mac/shop/models.py
+++ mac/shop/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_id = models.AutoField # Search Django Field Refrence on Google 
+    product_name = models.CharField(max_length=50)
+    desc = models.CharField(max_length=300)
+    pub_date = models.DateField()
