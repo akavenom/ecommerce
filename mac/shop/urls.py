5bd2190185a6af674eb9364e76f6751d55782143
Ojas Joshi <ojasjoshi.work@gmail.com>
2020-08-05 15:44:24 +0530

    Added Pipelines

--- mac/shop/urls.py
+++ mac/shop/urls.py
@@ -3,4 +3,15 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.index,  name="ShopHome"),
+    path("about/", views.about,  name="AboutUs"),
+    path("contact/", views.contact,  name="ContactUs"),
+    path("tracker/", views.tracker,  name="TrackingStatus"),
+    path("search/", views.search,  name="Search"),
+    path("productview/", views.productView,  name="ProductView"),
+    path("checkout/", views.checkout,  name="Checkout"),
+    path("login/", views.login,  name="Login"),
+    path("register/", views.register,  name="Register"),
+    path("cart/", views.cart,  name="Cart"),
+    
+
 ]
--- mac/shop/views.py
+++ mac/shop/views.py
@@ -4,3 +4,30 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'shop/index.html')
+
+def about(request):
+    return HttpResponse("We are at About")
+
+def contact(request):
+    return HttpResponse("We are at Contact")
+
+def tracker(request):
+    return HttpResponse("We are at Tracker")
+
+def search(request):
+    return HttpResponse("We are at Search")
+
+def productView(request):
+    return HttpResponse("We are at Product View")
+
+def checkout(request):
+    return HttpResponse("We are at Checkout")
+
+def login(request):
+    return HttpResponse("We are at Login")
+
+def register(request):
+    return HttpResponse("We are at Register")
+
+def cart(request):
+    return HttpResponse("We are at Cart")
